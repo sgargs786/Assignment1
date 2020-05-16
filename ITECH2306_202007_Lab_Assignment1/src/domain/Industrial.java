4ab6f7b715eeefd234ac9c51b6c9618402764405
sahilgarg <sahilgarg@sahils-mbp>
2020-05-17 01:56:28 +1000

    Added code for industrial property.

--- ITECH2306_202007_Lab_Assignment1/src/domain/Industrial.java
+++ ITECH2306_202007_Lab_Assignment1/src/domain/Industrial.java
@@ -1,30 +1,49 @@
 package domain;
 
 /**
- * @author Takeogh
  * @version 1.0
  * @created 02-Apr-2020 8:30:00am
  */
 public class Industrial extends Property {
 
-	private String hazardStatus;
-	private String heavyVehicleStatus;
-	
-	public Industrial() {
-		System.out.println("Not implemented yet");
-	}
+private String specialDescription;
+private static final double CIV_RATE = 0.0030;
+private static final double FIRE_SERVICES_BASE = 150;
+private static final double FIRE_SERVICES_PERCENT = 0.00006;
+private ServiceType fireServicesLevy;
 
-	@Override
-	public double calculateExtraServices() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public Industrial() {
+super();
+this.setSpecialDescription("None");
+setCapitalImprovedRate(CIV_RATE);
+}
 
-	@Override
-	public void setUpExtraServices() {
-		// TODO Auto-generated method stub
-		
-	}
+public String getSpecialDescription() {
+return specialDescription;
+}
 
-	
+public void setSpecialDescription(String specialDescription) {
+this.specialDescription = specialDescription;
+}
+
+@Override
+public void setUpExtraServices() {
+fireServicesLevy = new BaseAndPercentageOfValueService("Fire Levy",
+FIRE_SERVICES_BASE,
+FIRE_SERVICES_PERCENT,
+getCapitalImprovedValue());
+
+}
+
+@Override
+public double calculateExtraServices() {
+return fireServicesLevy.calculateChargeForServiceType();
+}
+
+@Override
+public String toString() {
+return super.toString() + "OtherProperty [\n" +
+ fireServicesLevy.toString() + "]\n "; 
+}
+
 }
